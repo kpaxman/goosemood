70d9f56fb0b43936421fdf3c7aed74fde20dd8d7
Amy Liang <amy.liaxx@gmail.com>
2017-12-24 20:45:16 -0500

    Update

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 var date = new Date();
 var month = date.getMonth() + 1;
 var day = date.getDate();
+
 var mood;
 var moodText = document.getElementById("moodText");
 var actionText = document.getElementById("actionText");
 
-if (month == 1) {
-    mood = "gone";
-} else if (month == 2 && date <= 15) {
-    mood = "gone";
-} else if (month == 2) {
+//Calculate mood by season
+if (month == 2 && date >= 15) {
     mood = "arriving"; 
 } else if (month == 3) {
     mood = "arriving";
-} else if (month == 4) {
+} else if (month == 4 || month == 5) {
     mood = "aggressive";
-} else if (month == 5) {
-    mood = "aggressive";     
 } else if (month >= 6 && month <= 8) {
     mood = "poopy";
 } else if (month >= 9 && month <= 11) {
@@ -26,9 +22,8 @@ if (month == 1) {
 } else {
     mood = "gone";
 }
-
-//var weather = 
     
+//Update display text
 if (mood == "gone") {
         moodText.textContent = "The geese have migrated South...";
         actionText.textContent = "You are safe for now."; 
@@ -45,28 +40,3 @@ if (mood == "gone") {
         moodText.textContent = "The geese are feeling OK.";
         actionText.textContent = "They'll hiss less (maybe) (on average).";  
 }
-
-
-
-/**
-
-TIMES:
-
-Feb to March: Arrival
-Mid-March to Early May: Breeding/Nesting
-Early May to Late July: Rearing/Mounting
-Late July to Mid Nov: Foraging Flights
-Mid Nov to Dec: Fall Migration
-
-http://thamesriver.on.ca/wp-content/uploads/NativeSpecies/geeseshorelines.pdf
-
-APRIL = bad time
-EARLY DECEMBER = mostly gone
-DECEMBER = gone
-
-
-TIPS:
-
-http://wdfw.wa.gov/living/canada_geese.html
-
-**/
