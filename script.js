2453905fd2ffca04bcd36c81136d7883bad63de3
Amy Liang <amy.liaxx@gmail.com>
2017-12-27 10:13:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var month = date.getMonth() + 1;
 var day = date.getDate();
 var mood;
 var moodText = document.getElementById("moodText");
+var gooseImg = document.getElementById("gooseImg");
 
 //Calculate mood by season
 if (month == 2 && date >= 15) {
@@ -24,13 +25,18 @@ if (month == 2 && date >= 15) {
     
 //Update display text
 if (mood == "gone") {
-        moodText.textContent = "The geese have migrated South... You are safe for now.";
+    moodText.textContent = "The geese have migrated South... You are safe for now.";
+    gooseImg.src = "img/goneGoose.png";
 } else if (mood == "arriving") {
-        moodText.textContent = "The geese are on their way back! Prepare yourself.";
+    moodText.textContent = "The geese are on their way back! Prepare yourself.";
+    gooseImg.src = "img/arrivingGoose.png";
 } else if (mood == "aggressive") {
-        moodText.textContent = "It's breeding season, and the geese are sensitive. Avoid interaction AT ALL COSTS.";
+    moodText.textContent = "It's breeding season, and the geese are sensitive. Avoid interaction AT ALL COSTS.";
+    gooseImg.src = "img/aggressiveGoose.png";
 } else if (mood == "poopy") {
-        moodText.textContent = "The geese are taking care of their ducklings. Watch out for green poop.";
+    moodText.textContent = "The geese are taking care of their ducklings. Watch out for green poop.";
+    gooseImg.src = "img/poopyGoose.png";
 } else  {
-        moodText.textContent = "The geese are feeling decent- but don't try cuddling up to one.";
+    moodText.textContent = "The geese are feeling decent- but don't try cuddling up to one.";
+    gooseImg.src = "img/okGoose.png";
 }
